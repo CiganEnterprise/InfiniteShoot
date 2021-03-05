3bfe94ea7d3e5fdbd69763f40dd3fa958668c6c2
Cigan <ciganoliviudavid@gmail.com>
2021-03-05 21:55:48 +0200

    Update gallery my_catalogue model

--- Gallery/views.py
+++ Gallery/views.py
@@ -94,6 +94,11 @@ class ChoosePreferredPhotosDetailView(LoginRequiredMixin, FormMixin, generic.Det
         return context
 
 
+    def get_number_of_instances_from_database(self, database_name, filter_name):
+
+        return database_name.objects.filter(image_positioning=filter_name).count()
+
+
     def post(self, request, *args, **kwargs):
         self.object = self.get_object()
 
@@ -125,19 +130,14 @@ def my_catalogue(request):
     template_name = 'gallery_details/my_catalogue.html'
 
     get_cover_image = ClientCatalogue.objects.filter(client=request.user, image_positioning='Cover Image')
-    get_image_one = ClientCatalogue.objects.filter(client=request.user, image_positioning='Content Image One')
-    get_image_two = ClientCatalogue.objects.filter(client=request.user, image_positioning='Content Image Two')
-    get_image_three = ClientCatalogue.objects.filter(client=request.user, image_positioning='Content Image Three')
-    get_image_four = ClientCatalogue.objects.filter(client=request.user, image_positioning='Content Image Four')
+    get_image = ClientCatalogue.objects.filter(client=request.user, image_positioning='Content Image')
     get_back_image = ClientCatalogue.objects.filter(client=request.user, image_positioning='Back Image')
 
     context = {
         'get_cover_image': get_cover_image,
-        'get_image_one': get_image_one,
-        'get_image_two': get_image_two,
-        'get_image_three': get_image_three,
-        'get_image_four': get_image_four,
+        'get_image': get_image,
         'get_back_image': get_back_image,
     }
 
     return render(request, template_name, context)
+
